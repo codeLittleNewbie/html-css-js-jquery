4aaf7b04d2b05f99ad98067584c917c1aba354e4
李炜强 <W-Q@weiqiangdeMacBook-Pro.local>
2017-07-03 10:54:34 +0800

    原生Ajax封装

---
+++ 04-jQuery/03-Ajax/Ajax原理day02.md
@@ -0,0 +1,2 @@
+### jQuery-Ajax注意点
+#### 1.jQuery发送网络请求
---
+++ 04-jQuery/03-Ajax/原生Ajax封装.js
@@ -0,0 +1,68 @@
+/**
+ * Created by W-Q on 2017/7/3.
+ */
+function ajax(data) {
+    // 0.获取必须参数
+    var method = data.type ? data.type : "get";
+    var url = data.url;
+    var paramStr = null;
+    var paramArr = [];
+    var timerout = data.timeout ? data.timeout : 1000;
+    var t = new Date().getTime();
+    var timer = null;
+
+    // 1.创建XMLHTTPRequest对象
+    if (window.XMLHttpRequest) {
+        var xhr = new XMLHttpRequest();
+    } else {
+        var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // 2.对参数进行处理
+    if (data.data) {
+        for (var attr in data.data) {
+            paramArr.push(attr + "=" + data.data[attr]);
+        }
+        paramArr.push("t=" + t);
+
+        paramStr = paramArr.join("&");
+    }
+
+    // 3.配置xml对象
+    if (method == "get") {
+        // 对url进行编码(中文处理)
+        url = encodeURI(url);
+
+        if (paramStr) {
+            url += "?" + paramStr;
+        }
+        xhr.open(method, url, true);
+        xhr.send(null);
+    } else {
+
+
+        xhr.open(method, url, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(paramStr)
+    }
+
+    // 4.监听xhr状态的改变
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                // 清除定时器
+                clearInterval(timer);
+                data.success(xhr.responseText);
+
+            } else {
+                data.error("请求错误");
+            }
+        }
+    };
+
+    // 5.设置定时器
+    timer = setInterval(function () {
+        // 取消请求
+        xhr.abort();
+    }, timerout);
+}
